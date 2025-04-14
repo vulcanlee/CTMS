0fb18fd2b1a886a204ba32fa3f6c44b0b28bbd63
Vulcan <vulcan.lee@gmail.com>
2025-04-14 10:47:42 +0800

    0

--- src/CTMS/CTMS.DataModel/Models/ClinicalInformation/臨床資料.cs
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/臨床資料.cs
@@ -10,5 +10,6 @@ namespace CTMS.DataModel.Models.ClinicalInformation
     public class 臨床資料
     {
         public 臨床資料手術 臨床資料手術 { get; set; } = new();
+        public 臨床資料病理報告 臨床資料病理報告 { get; set; } = new();
     }
 }
---
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/臨床資料病理報告.cs
@@ -0,0 +1,35 @@
+﻿using CTMS.Share.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CTMS.DataModel.Models.ClinicalInformation
+{
+    public class 臨床資料病理報告
+    {
+        public string VisitCode { get; set; }
+        public string AssessmentDate { get; set; }
+        public string SubjectNo { get; set; }
+        public string 切片日期 { get; set; }
+        public string Histology { get; set; }
+        public string TnmStage { get; set; }
+        public string Myometrium { get; set; }
+        public string UterineSerosaInvolvement        { get; set; }
+        public string BloodLymphaticVesselInvasion { get; set; }
+        public string Cervix { get; set; }
+        public string Parametrium { get; set; }
+        public string OvaryRight { get; set; }
+        public string OvaryLeft { get; set; }
+        public string FallopianTubeRight { get; set; }
+        public string FallopianTubeLeft { get; set; }
+        public string Vagina { get; set; }
+        public string RegionalLymphNodes { get; set; }
+        public string IsolatedTumorCells { get; set; }
+        public string AdditionalPathologicalFindings { get; set; }
+        public string ImmunohistochemicalTest  { get; set; }
+
+    }
+}
