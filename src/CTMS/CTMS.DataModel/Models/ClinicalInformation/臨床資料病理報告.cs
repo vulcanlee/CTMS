fe2410cef062acad45d1e13c62b083bfb1c05a91
Vulcan <vulcan.lee@gmail.com>
2025-04-15 22:19:56 +0800

    更新臨床報告與手術視圖的輸入元件
    在 `ClinicalPathologyReportView.razor` 中，移除左側輸卵管的文本框，新增右側輸卵管的文本框，並根據編輯模式顯示相應的輸入或值。
    
    在 `ClinicalSurgeryView.razor` 中，移除「Residual tumor」標題，保留編輯模式的下拉選單邏輯。

--- src/CTMS/CTMS.DataModel/Models/ClinicalInformation/臨床資料病理報告.cs
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/臨床資料病理報告.cs
@@ -10,10 +10,10 @@ namespace CTMS.DataModel.Models.ClinicalInformation
 {
     public class 臨床資料病理報告
     {
-        public string VisitCode { get; set; }
+        public string VisitCode { get; set; } 
         public DateTime AssessmentDate { get; set; }
         public string SubjectNo { get; set; }
-        public string 切片日期 { get; set; }
+        public DateTime 切片日期 { get; set; }
         public string Histology { get; set; }
         public string TnmStage { get; set; }
         public string Myometrium { get; set; }
