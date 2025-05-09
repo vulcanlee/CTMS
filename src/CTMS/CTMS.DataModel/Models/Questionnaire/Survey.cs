b38ef7159fced6b13d1611c5166fe95e375ebd04
Vulcan <vulcan.lee@gmail.com>
2025-05-09 14:29:10 +0800

    新增調查問卷結構及相關問題
    新增 `Survey.cs` 和 `SurveyWhooqol.cs` 檔案，定義調查問卷的結構，包括 `Survey`、`Question`、`Option` 和 `VisibilityCondition` 類別，並使用 `JsonPropertyName` 屬性指定 JSON 中的屬性名稱。
    
    在 `whooqol問卷.json` 及其他 JSON 檔案中，新增 WHOQOL-BREF 問卷、化療副作用問卷、放療副作用問卷及標靶副作用問卷的具體問題和選項，涵蓋生活品質、健康狀況及治療副作用，並包含可見性條件以控制問題顯示。

---
+++ src/CTMS/CTMS.DataModel/Models/Questionnaire/Survey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CTMS.DataModel.Models.Questionnaire;
+
+public class Survey
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("questions")]
+    public List<Question> Questions { get; set; }
+}
+
+public class Question
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("options")]
+    public List<Option>? Options { get; set; }
+
+    [JsonPropertyName("visibilityCondition")]
+    public VisibilityCondition? VisibilityCondition { get; set; }
+}
+
+public class Option
+{
+    [JsonPropertyName("value")]
+    public int Value { get; set; }
+
+    [JsonPropertyName("label")]
+    public string Label { get; set; }
+}
+
+public class VisibilityCondition
+{
+    [JsonPropertyName("questionId")]
+    public string QuestionId { get; set; }
+
+    [JsonPropertyName("anyOf")]
+    public List<int> AnyOf { get; set; }
+}
---
+++ src/CTMS/CTMS.DataModel/Models/Questionnaire/SurveyWhooqol.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CTMS.DataModel.Models.Questionnaire;
+
+public class SurveyWhooqol
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("formLabel")]
+    public string FormLabel { get; set; }
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("questionLabel")]
+    public string QuestionLabel { get; set; }
+
+    [JsonPropertyName("questions")]
+    public List<Question> Questions { get; set; }
+}
+
+public class QuestionWhooqol
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("options")]
+    public List<Option> Options { get; set; }
+}
+
+public class OptionWhooqol
+{
+    [JsonPropertyName("value")]
+    public int Value { get; set; }
+
+    [JsonPropertyName("label")]
+    public string Label { get; set; }
+}
