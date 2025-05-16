0ceed34a4a4228b31d78ea431c3255be7ef76b70
Vulcan <vulcan.lee@gmail.com>
2025-05-16 22:04:38 +0800

    .

---
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/抽血檢驗.cs
@@ -0,0 +1,9 @@
+﻿namespace CTMS.DataModel.Models.ClinicalInformation
+{
+    public class 抽血檢驗
+    {
+        public List<檢驗項目> 抽血檢驗血液 { get; set; } = new();
+        public List<檢驗項目> 抽血檢驗生化 { get; set; } = new();
+        public List<檢驗項目> 抽血檢驗生化2 { get; set; } = new();
+    }
+}
---
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/檢驗項目.cs
@@ -0,0 +1,18 @@
+﻿using CTMS.Share.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTMS.DataModel.Models.ClinicalInformation
+{
+    public class 檢驗項目
+    {
+        public string 項目名稱 { get; set; }
+        public string 參考區間 { get; set; }
+        public string 檢驗數值 { get; set; }
+        public DateTime? SamplingDate { get; set; }
+
+    }
+}
