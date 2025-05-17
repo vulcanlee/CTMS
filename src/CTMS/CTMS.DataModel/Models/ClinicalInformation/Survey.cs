fe24f9e0915916c0b8b17450c3bbb4b56428d727
Vulcan <vulcan.lee@gmail.com>
2025-05-17 20:42:28 +0800

    新增 Survey、Question、Option 和 VisibilityCondition 類別
    此提交新增了 `Survey` 類別，包含 `Title` 和 `Questions` 屬性，`Questions` 是 `Question` 類別的列表。`Question` 類別包含 `Id`、`Type`、`Text`、`Options` 和 `VisibilityCondition` 屬性，其中 `Options` 是可選的 `Option` 類別列表，`VisibilityCondition` 也是可選的。還新增了 `Option` 和 `VisibilityCondition` 類別，並引入了必要的命名空間。

---
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/Survey.cs
@@ -0,0 +1,36 @@
+﻿using CTMS.Share.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTMS.DataModel.Models.ClinicalInformation
+{
+    public class Survey
+    {
+        public string Title { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+
+    public class Question
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Text { get; set; }
+        public List<Option>? Options { get; set; }
+        public VisibilityCondition? VisibilityCondition { get; set; }
+    }
+
+    public class Option
+    {
+        public int Value { get; set; }
+        public string Label { get; set; }
+    }
+
+    public class VisibilityCondition
+    {
+        public string QuestionId { get; set; }
+        public List<int> AnyOf { get; set; }
+    }
+}
