faca4ac1d258f883e458ccda24b8c26bada0f917
Vulcan <vulcan.lee@gmail.com>
2025-07-17 21:55:59 +0800

    重構檢查項目結構並改善用戶界面
    在 `OtherTreatmentImageNode.cs` 中，將 `Items` 屬性替換為 `Item`，並新增多個日期屬性以記錄檢查日期。
    在 `OtherImageView.razor` 中，移除新增紀錄的按鈕，調整表格結構，並新增日期選擇器以顯示和編輯檢查日期。
    更新樣式文件 `OtherImageView.razor.css`，改善表格的可讀性和美觀性。
    這些變更簡化了程式碼結構並提升了用戶界面的可用性。

--- src/CTMS/CTMS.DataModel/Models/ClinicalInformation/OtherTreatmentImageNode.cs
+++ src/CTMS/CTMS.DataModel/Models/ClinicalInformation/OtherTreatmentImageNode.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CTMS.DataModel.Models.ClinicalInformation
 {
@@ -12,19 +13,23 @@ namespace CTMS.DataModel.Models.ClinicalInformation
     {
         public VisitCodeModel VisitCode { get; set; } = new();
         public string SubjectNo { get; set; }
-        public List<OtherTreatmentImageItem> Items { get; set; } = new();
+        public OtherTreatmentImageItem Item { get; set; } = new();
     }
 
     public class OtherTreatmentImageItem
     {
         public string First { get; set; }
-        public DateTime Visit { get; set; }
         public string ChestXRay { get; set; }
         public string LeadEKG12 { get; set; }
         public string ChestCT { get; set; }
         public string AbdCT { get; set; }
         public string BrainMRI { get; set; }
         public string BoneScan { get; set; }
-        public string Other { get; set; }
+        public DateTime? ChestXRayDate { get; set; }
+        public DateTime? LeadEKG12Date { get; set; }
+        public DateTime? ChestCTDate { get; set; }
+        public DateTime? AbdCTDate { get; set; }
+        public DateTime? BrainMRIDate { get; set; }
+        public DateTime? BoneScanDate { get; set; }
     }
 }
