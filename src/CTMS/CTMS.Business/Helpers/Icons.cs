8055dc5ab1fd7852299b96eb65ea7e88504fded0
Vulcan <vulcan.lee@gmail.com>
2026-02-02 13:24:48 +0800

    新增問卷分頁圓形圖示及相關工具類別
    - 在化療副作用分頁新增紅色圓形 SVG 圖示
    - 新增 Icons 類別，支援多色圓形 SVG 產生
    - 新增 SurveyTabDisplayModel，加入 CircleIcon 屬性

---
+++ src/CTMS/CTMS.Business/Helpers/Icons.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.Drawing;
+
+namespace CTMS.Helper;
+
+public class Icons
+{
+    public const string Circle_Red = """
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" role="img">
+  <circle cx="12" cy="12" r="12" fill="#ff0000"/>
+</svg>
+
+""";
+
+    public string GetCircleIcon(Syncfusion.Drawing.Color color)
+    {
+        //紅燈:
+        //有新增頁面但尚未填入任何資料
+        //黃燈:
+        //有新增頁面填入部分資料，完成度50 %
+        //綠燈:
+        //所有新增頁面皆完成100 %
+        //灰燈:
+        //尚未新增頁面也沒有填入資料  
+        string result = "";
+        if (color == Syncfusion.Drawing.Color.Red)
+        {
+            result = Circle_Red;
+        }
+        else if (color == Syncfusion.Drawing.Color.Green)
+        {
+            result = Circle_Red.Replace("#ff0000", "#00ff00");
+        }
+        else if (color == Syncfusion.Drawing.Color.Yellow)
+        {
+            result = Circle_Red.Replace("#ff0000", "#ffff00");
+        }
+        else if (color == Syncfusion.Drawing.Color.Gray)
+        {
+            result = Circle_Red.Replace("#ff0000", "#808080");
+        }
+        return result;
+    }
+}
---
+++ src/CTMS/CTMS.DataModel/Models/Systems/SurveyTabDisplayModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CTMS.DataModel.Models.Systems;
+
+public class SurveyTabDisplayModel
+{
+    public string CircleIcon { get; set; } = string.Empty;
+}
