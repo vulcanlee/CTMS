5e83bdc5cb954a46d9f24320e238bc8befea8a84
Vulcan <vulcan.lee@gmail.com>
2026-02-25 15:48:56 +0800

    新增 ExportPage 頁面與基本資料匯出功能
    新增 ExportPage（/Export）頁面，包含「Export Data」按鈕。實作後端邏輯，注入 PatientService，點擊按鈕時可取得指定病患資料並轉換為 patientData，目前尚未實作實際匯出檔案功能。

---
+++ src/CTMS/CTMS/Components/Pages/AdminTools/ExportPage.razor.cs
@@ -0,0 +1,20 @@
+﻿using CTMS.AdapterModels;
+using CTMS.DataModel.Models.ClinicalInformation;
+using CTMS.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace CTMS.Components.Pages.AdminTools;
+
+public partial class ExportPage
+{
+    [Inject]
+    public PatientService PatientService { get; set; }
+    PatientAdapterModel patientAdapterModel = new();
+    public PatientData patientData { get; set; }= new();
+    public async Task ExportData()
+    {
+
+        patientAdapterModel = await PatientService.GetAsync("ddabf6f3-08a4-4ed4-8031-9f05fdffc3c3");
+        patientData.FromJson(patientAdapterModel.JsonData);
+    }
+}
